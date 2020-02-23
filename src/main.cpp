970f24867ab3a59463951c9fa4deb0a357e45607
pras <prastya.ismet@gmail.com>
2020-02-24 02:09:17 +0700

    test1

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
 
 void setup() {
   // put your setup code here, to run once:
