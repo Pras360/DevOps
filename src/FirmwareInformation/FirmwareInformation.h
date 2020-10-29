e2bc33c37c1470e395a69c35dd116ba032362efe
Firman Adi Wicaksono <36994892+firmanadiwicaksono@users.noreply.github.com>
2020-10-30 02:07:36 +0700

    test release 3

--- src/FirmwareInformation/FirmwareInformation.h
+++ src/FirmwareInformation/FirmwareInformation.h
@@ -3,7 +3,7 @@
 
 #include <Arduino.h>
 
-const String firmware_name = "Lampu Jalam Release 2";
+const String firmware_name = "Lampu Jalam Release 3";
 const String firmware_description = "Lampu Jalan Gemoyyy";
 const String firmware_version = "0.0.3";
 const String legal_copyright = "Copyright (c) Gemoyyy 2020";
