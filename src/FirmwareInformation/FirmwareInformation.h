f8cba5ba58a79e6e03ce850497bc8a60bb54ff8a
pras <prastya.ismet@gmail.com>
2020-10-31 22:05:31 +0700

    phase 2

--- src/FirmwareInformation/FirmwareInformation.h
+++ src/FirmwareInformation/FirmwareInformation.h
@@ -3,10 +3,10 @@
 
 #include <Arduino.h>
 
-const String firmware_name = "Lampu Jalam Release 1";
-const String firmware_description = "Penambahan metode DevOps pada sistem";
-const String firmware_version = "0.0.1";
-const String legal_copyright = "Copyright (c) Prastya Naufal Abdillah 2020";
+const String firmware_name = "Lampu Jalam Release 2";
+const String firmware_description = "Penambahan topologi mesh";
+const String firmware_version = "0.0.2";
+const String legal_copyright = "Copyright (c) Prastya Naufal Abdillah";
 const String company_name = "Pras360";
 const String firmware_type = "Update Version";
 
